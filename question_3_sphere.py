f005cd5e9574c4d5f80f9c5b340d9b881ec2a68d
Charissa-Anne-Claudius <163951629+Charissa-Anne-Claudius@users.noreply.github.com>
2024-10-15 09:22:31 +0800

    Update question_3_sphere.py

--- question_3_sphere.py
+++ question_3_sphere.py
@@ -6,14 +6,13 @@
 from math import pi
 
 # kira_sfera() is a function that return the volume of sphere
-def volume_sphere(.........):
-    volume = 4/3 * ......... * radius ** .........
-    return .........
-
+def volume_sphere(r):
+    volume = 4/3 * *pi * r**3 * radius ** r
+    return volume_sphere V = 4/3 
 def main():
     r = float(input("Input radius: ")) 
-    Volume = ......... (r) 
-    print(f"Isipadu sfera = { ......... :.2f}") 
+    Volume = 4/3 (r) 
+    print(f"Isipadu sfera = {  r, 3 =2145.50:.2f}") 
 
 # Don't change the code below!
 if __name__ == "__main__":
